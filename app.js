80c08d1c48a189a8ffb31a601f83823ccca18043
Muhammad Anas <Anasnaseem420@gmail.com>
2023-10-12 09:31:02 +0500

    Spelling mistake fix & Change Words

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 document.getElementById('noBtn').addEventListener('click', (e) => {
   let vertical = Math.floor(Math.random() * (85 - -56) + -56)
-  let hirozontal = Math.floor(Math.random() * (74 - -24) + -24)
-  e.target.style.transform = `translate(${hirozontal}px, ${vertical}px)`
+  let horizontal = Math.floor(Math.random() * (74 - -24) + -24)
+  e.target.style.transform = `translate(${horizontal}px, ${vertical}px)`
 
 })
 
 document.getElementById('yesBtn').addEventListener('click', (e) => {
   e.target.parentNode.style.display = 'none';
   document.querySelector('img').style.display = 'block';
-  document.querySelector('h3').innerText = 'Mujhe Pata Tha !';
+  document.querySelector('h3').innerText = 'I Knew It !';
 
 })
